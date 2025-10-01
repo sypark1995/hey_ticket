9b740b1e21d82e9a9ba0f53dff5b80d1204d1a9e
sypark1995 <115516297+sypark1995@users.noreply.github.com>
2025-10-01 16:21:48 +0900

    Add : Jsoup crawling 라이브러리 적용

--- app/src/main/java/com/sypark/openTicket/MainActivity.kt
+++ app/src/main/java/com/sypark/openTicket/MainActivity.kt
@@ -2,10 +2,50 @@ package com.sypark.openTicket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import com.sypark.openTicket.dao.InterParkDao
+import com.sypark.openTicket.network.BaseUrlUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.jsoup.Jsoup
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn)
+
+        btn.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.IO) {
+                getInterParkData("0")
+            }
+        }
+    }
+
+    val interParkList = ArrayList<InterParkDao>()
+
+    private fun getInterParkData(page: String) {
+        val lastUrl = "bbsno=0&pageno=${page}&stext=&KindOfGoods=&Genre=&sort="
+        val url = BaseUrlUtil.interParkSite + lastUrl
+
+        try {
+//            val doc = Jsoup.connect(url).timeout(1000 * 10).get()
+            val doc = Jsoup.parse(URL(url).openStream(), "euc-kr", url)
+
+            val data = doc.select(".table").select("div").select("tbody").select("tr")
+            var type = ""
+            for (item in data) {
+                type = item.select(".type").text()
+            }
+            Log.e("!!!", type)
+            Toast.makeText(this, type, Toast.LENGTH_LONG).show()
+        } catch (e: java.lang.Exception) {
+
+        }
     }
 }
---
+++ app/src/main/java/com/sypark/openTicket/dao/InterParkDao.kt
@@ -0,0 +1,12 @@
+package com.sypark.openTicket.dao
+
+data class InterParkDao(
+
+    val type: String,
+
+    val subject: String,
+
+    val date: String,
+
+    val count: String
+)
