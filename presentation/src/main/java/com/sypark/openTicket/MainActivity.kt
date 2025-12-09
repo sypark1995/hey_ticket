3cc04f71b7e827e9f917af62771f15b1417c2a11
sypark <115516297+sypark1995@users.noreply.github.com>
2025-12-09 17:59:56 +0900

    Add : 비로그인 / 로그인 시 처음 진입 startDestination 분기 처리 완료

--- presentation/src/main/java/com/sypark/openTicket/MainActivity.kt
+++ presentation/src/main/java/com/sypark/openTicket/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.sypark.openTicket
 
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.sypark.openTicket.base.BaseActivity
 import com.sypark.openTicket.databinding.ActivityMainBinding
+import com.sypark.openTicket.view.MainFragment
+import com.sypark.openTicket.view.RecommendFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,6 +15,21 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
         binding.lifecycleOwner = this
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val navGraph = navController.navInflater.inflate(R.navigation.nav_graph)
+
+        val isLogin = true
+
+        if (isLogin) {
+            navGraph.setStartDestination(R.id.mainFragment)
+        } else {
+            navGraph.setStartDestination(R.id.recommendFragment)
+        }
+
+        navController.graph = navGraph
     }
 
 }
---
+++ presentation/src/main/java/com/sypark/openTicket/view/RecommendFragment.kt
@@ -0,0 +1,16 @@
+package com.sypark.openTicket.view
+
+import android.view.View
+import com.sypark.openTicket.R
+import com.sypark.openTicket.base.BaseFragment
+import com.sypark.openTicket.databinding.FragmentRecommendBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RecommendFragment : BaseFragment<FragmentRecommendBinding>(R.layout.fragment_recommend) {
+
+    override fun init(view: View) {
+
+    }
+
+}
