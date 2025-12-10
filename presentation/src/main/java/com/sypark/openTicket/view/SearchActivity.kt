b570cbe865ce37edb97d50ac8173556a85c3a034
sypark <115516297+sypark1995@users.noreply.github.com>
2025-12-10 20:08:43 +0900

    Add : Hilt 어노테이션 추가

--- presentation/src/main/java/com/sypark/openTicket/view/SearchActivity.kt
+++ presentation/src/main/java/com/sypark/openTicket/view/SearchActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import com.sypark.openTicket.R
 import com.sypark.openTicket.base.BaseActivity
 import com.sypark.openTicket.databinding.ActivitySearchBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SearchActivity : BaseActivity<ActivitySearchBinding>(R.layout.activity_search) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- presentation/src/main/java/com/sypark/openTicket/view/SearchPerformanceFragment.kt
+++ presentation/src/main/java/com/sypark/openTicket/view/SearchPerformanceFragment.kt
@@ -4,10 +4,12 @@ import android.view.View
 import com.sypark.openTicket.R
 import com.sypark.openTicket.base.BaseFragment
 import com.sypark.openTicket.databinding.FragmentSearchPerformanceBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SearchPerformanceFragment :
     BaseFragment<FragmentSearchPerformanceBinding>(R.layout.fragment_search_performance) {
     override fun init(view: View) {
-        TODO("Not yet implemented")
+
     }
 }
