da675643522d21cac0efc5dbb78cec8229044b6f
sypark <115516297+sypark1995@users.noreply.github.com>
2025-12-13 21:10:11 +0900

    Add : CategoryDetailFragment.kt -> CategoryFragment back 버튼 구현 완료

--- presentation/src/main/java/com/sypark/openTicket/view/fragments/CategoryDetailFragment.kt
+++ presentation/src/main/java/com/sypark/openTicket/view/fragments/CategoryDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.sypark.openTicket.view.fragments
 
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.sypark.openTicket.R
 import com.sypark.openTicket.base.BaseFragment
 import com.sypark.openTicket.databinding.FragmentCategoryDetailBinding
@@ -11,5 +12,8 @@ class CategoryDetailFragment :
     BaseFragment<FragmentCategoryDetailBinding>(R.layout.fragment_category_detail) {
     override fun init(view: View) {
 
+        binding.imgBack.setOnClickListener {
+            findNavController().navigate(CategoryDetailFragmentDirections.actionCategoryDetailFragmentToCategoryFragment())
+        }
     }
 }
