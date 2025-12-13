2db99277a9a39dd1fbd65b8c5f7c71368dfc2f5e
sypark <115516297+sypark1995@users.noreply.github.com>
2025-12-13 10:14:09 +0900

    reformat

--- presentation/src/main/java/com/sypark/openTicket/view/SearchActivity.kt
+++ presentation/src/main/java/com/sypark/openTicket/view/SearchActivity.kt
@@ -1,16 +1,11 @@
 package com.sypark.openTicket.view
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.viewModels
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.sypark.openTicket.R
 import com.sypark.openTicket.base.BaseActivity
 import com.sypark.openTicket.databinding.ActivitySearchBinding
-import com.sypark.openTicket.model.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
