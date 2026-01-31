258688c2dd6ab3f3e19224cfe48ce91bdc2469f5
sypark <115516297+sypark1995@users.noreply.github.com>
2026-01-31 21:22:52 +0900

    Add : recyclerview item create(xml)

---
+++ data/src/main/java/com/sypark/data/db/entity/Detail.kt
@@ -0,0 +1,8 @@
+package com.sypark.data.db.entity
+
+import com.google.gson.annotations.SerializedName
+
+data class Detail(
+    @SerializedName("url")
+    var url: String
+)
---
+++ data/src/main/java/com/sypark/data/db/entity/Ticket.kt
@@ -0,0 +1,66 @@
+package com.sypark.data.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "ticket_table")
+data class Ticket(
+    @PrimaryKey
+    @SerializedName("mt20id")
+    var mt20id: String,
+
+    @SerializedName("mt10id")
+    var mt10id: String,
+
+    @SerializedName("title")
+    var title: String,
+
+    @SerializedName("startDate")
+    var startDate: String,
+
+    @SerializedName("endDate")
+    var endDate: String,
+
+    @SerializedName("place")
+    var place: String,
+
+    @SerializedName("cast")
+    var cast: String,
+
+    @SerializedName("crew")
+    var crew: String,
+
+    @SerializedName("runtime")
+    var runtime: String,
+
+    @SerializedName("age")
+    var age: String,
+
+    @SerializedName("entrpsnm")
+    var entrpsnm: String,
+
+    @SerializedName("pcseguidance")
+    var pcseguidance: String,
+
+    @SerializedName("poster")
+    var poster: String,
+
+    @SerializedName("sty")
+    var sty: String,
+
+    @SerializedName("genre")
+    var genre: String,
+
+    @SerializedName("state")
+    var state: String,
+
+    @SerializedName("openrun")
+    var openrun: String,
+
+    @SerializedName("styurls")
+    var styurls: ArrayList<Detail>,
+
+    @SerializedName("dtguidance")
+    var dtguidance: String
+)
