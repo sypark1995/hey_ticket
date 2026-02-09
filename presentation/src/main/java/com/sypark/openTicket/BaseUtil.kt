e2c9fe11dc7d50b7904780caa27faf3eec3fde08
sypark1995 <115516297+sypark1995@users.noreply.github.com>
2026-02-09 20:41:30 +0900

    Fix : Ticket detail xml 수정 중

--- presentation/src/main/java/com/sypark/openTicket/BaseUtil.kt
+++ presentation/src/main/java/com/sypark/openTicket/BaseUtil.kt
@@ -1,14 +1,19 @@
 package com.sypark.openTicket
 
+import android.content.Context
+import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import android.graphics.Matrix
+import android.graphics.drawable.BitmapDrawable
 import android.util.Log
+import java.lang.Boolean
 import java.net.HttpURLConnection
 import java.net.URL
 
 object BaseUtil {
-    fun convertBitmapFromURL(url: String): Bitmap? {
+    fun convertBitmapFromURL(url: String, reqWidth: Int, reqHeight: Int): Bitmap? {
         try {
             val url = URL(url)
             val connection = url.openConnection() as HttpURLConnection
@@ -16,12 +21,37 @@ object BaseUtil {
             connection.connect()
             val input = connection.inputStream
             val bitmap = BitmapFactory.decodeStream(input)
+            BitmapFactory.Options().run {
+                inSampleSize = calculateInSampleSize(this, reqWidth, reqHeight)
+                inJustDecodeBounds = false
+            }
 
             return bitmap
         } catch (e: Exception) {
             Log.e("convertBitmapFromURL", e.toString())
         }
         return null
+    }
+
+
+    fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+        // Raw height and width of image
+        val (height: Int, width: Int) = options.run { outHeight to outWidth }
+        var inSampleSize = 1
+
+        if (height > reqHeight || width > reqWidth) {
+
+            val halfHeight: Int = height / 2
+            val halfWidth: Int = width / 2
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+
+        return inSampleSize
     }
 
     fun mergeBitmapsVertical(bitmaps: List<Bitmap>): Bitmap {
@@ -31,8 +61,8 @@ object BaseUtil {
             it.width
         } ?: 0
         val totalHeight = bitmaps.sumOf { it.height }
-        Log.e("totalWidth",totalWidth.toString())
-        Log.e("totalHeight",totalHeight.toString())
+        Log.e("totalWidth", totalWidth.toString())
+        Log.e("totalHeight", totalHeight.toString())
         // 새로운 비트맵을 생성합니다.
         val mergedBitmap = Bitmap.createBitmap(totalWidth, totalHeight, Bitmap.Config.ARGB_8888)
 
@@ -45,5 +75,48 @@ object BaseUtil {
         }
 
         return mergedBitmap
+    }
+
+    val Int.dp: Int
+        get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
+
+    fun bitmapResizePrc(
+        context: Context,
+        Src: Bitmap,
+        newHeight: Int,
+        newWidth: Int
+    ): BitmapDrawable {
+        val result: BitmapDrawable
+        var width = Src.width
+        var height = Src.height
+
+        // calculate the scale - in this case = 0.4f
+        val scaleWidth = newWidth.toFloat() / width
+        val scaleHeight = newHeight.toFloat() / height
+
+        // createa matrix for the manipulation
+        val matrix = Matrix()
+        // resize the bit map
+        matrix.postScale(scaleWidth, scaleHeight)
+
+        // rotate the Bitmap  회전 시키려면 주석 해제!
+        //matrix.postRotate(45);
+
+        // recreate the new Bitmap
+        val resizedBitmap = Bitmap.createBitmap(Src, 0, 0, width, height, matrix, true)
+
+        // check
+        width = resizedBitmap.width
+        height = resizedBitmap.height
+        Log.i(
+            "ImageResize", "Image Resize Result : " + Boolean.toString(
+                newHeight == height && newWidth == width
+            )
+        )
+
+        // make a Drawable from Bitmap to allow to set the BitMap
+        // to the ImageView, ImageButton or what ever
+        result = BitmapDrawable(context.resources, resizedBitmap)
+        return result
     }
 }
