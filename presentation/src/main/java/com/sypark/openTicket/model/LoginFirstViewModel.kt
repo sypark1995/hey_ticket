ca6d62b4d223c4548fbca69510176eec4b947823
sypark1995 <115516297+sypark1995@users.noreply.github.com>
2026-03-08 19:35:25 +0900

    feat : LoginFragment 검증 첫번째 뷰 create

---
+++ presentation/src/main/java/com/sypark/openTicket/model/LoginFirstViewModel.kt
@@ -0,0 +1,24 @@
+package com.sypark.openTicket.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.sypark.openTicket.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginFirstViewModel @Inject constructor(
+) : BaseViewModel() {
+
+    var mutableEmailAddress = MutableLiveData<String>()
+    var emailAddress: LiveData<String>
+        get() = mutableEmailAddress
+
+    init {
+        emailAddress = mutableEmailAddress
+    }
+
+    fun setEmailAddress(email: String) {
+        mutableEmailAddress.postValue(email)
+    }
+}
--- presentation/src/main/java/com/sypark/openTicket/view/fragments/LoginFirstFragment.kt
+++ presentation/src/main/java/com/sypark/openTicket/view/fragments/LoginFirstFragment.kt
@@ -2,46 +2,72 @@ package com.sypark.openTicket.view.fragments
 
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.sypark.openTicket.R
 import com.sypark.openTicket.base.BaseFragment
 import com.sypark.openTicket.databinding.FragmentLoginFirstBinding
+import com.sypark.openTicket.model.LoginFirstViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFirstFragment : BaseFragment<FragmentLoginFirstBinding>(R.layout.fragment_login_first) {
 
+    private val viewModel: LoginFirstViewModel by viewModels()
+
     override fun init(view: View) {
+
+        viewModel.emailAddress.observe(viewLifecycleOwner) {
+            if (it.isEmpty()) {
+                binding.layoutLoginEdit.setBackgroundResource(R.drawable.round_12_gray_white)
+                binding.textEmailError.visibility = View.GONE
+
+                binding.btnEmail.setBackgroundResource(R.drawable.round_12_gray)
+                binding.btnEmail.isEnabled = false
+            } else {
+                if (android.util.Patterns.EMAIL_ADDRESS.matcher(it).matches()) {
+                    //email 맞음!
+                    binding.layoutLoginEdit.setBackgroundResource(R.drawable.round_12_gray_white)
+
+                    binding.btnEmail.setBackgroundResource(R.drawable.round_12_black)
+                    binding.btnEmail.isEnabled = true
+
+                    binding.textEmailError.visibility = View.GONE
+                } else {
+                    //email 아님
+                    binding.layoutLoginEdit.setBackgroundResource(R.drawable.round_12_red_white)
+                    binding.textEmailError.visibility = View.VISIBLE
+
+                    binding.btnEmail.setBackgroundResource(R.drawable.round_12_gray)
+                    binding.btnEmail.isEnabled = false
+                }
+            }
+        }
+
         binding.layoutLoginTop.imgBack.setOnClickListener {
             findNavController().popBackStack()
+        }
+
+        binding.btnEmail.setOnClickListener {
+            findNavController()
         }
         binding.editEmail.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                s.toString().let {
-                    if (s != null) {
-                        if (android.util.Patterns.EMAIL_ADDRESS.matcher(s).matches()) {
-                            //email 맞음!
-                            binding.layoutLoginEdit.isErrorEnabled = false
-                        } else {
-                            Log.e("!!!!",s.toString())
-                            //email 아님!
-                            binding.layoutLoginEdit.isErrorEnabled = true
-                            binding.layoutLoginEdit.error = "올바른 이메일을 입력해주세요"
-                        }
-                    }
-                }
+                viewModel.setEmailAddress(s.toString())
             }
 
             override fun afterTextChanged(s: Editable?) {
 
             }
-
         })
+
+
+
+
     }
 
 }
