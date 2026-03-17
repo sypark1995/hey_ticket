10018afc1d0059078b17f98fe6ef442a6782e61a
sypark1995 <115516297+sypark1995@users.noreply.github.com>
2026-03-17 20:44:13 +0900

    feat : 관심 카테고리 area UI create (70%) , flexbox 라이브러리 사용

---
+++ presentation/src/main/java/com/sypark/openTicket/view/adapter/RecommendAreaAdapter.kt
@@ -0,0 +1,90 @@
+package com.sypark.openTicket.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.sypark.data.db.entity.CategoryDetailArea
+import com.sypark.openTicket.R
+import com.sypark.openTicket.databinding.ItemRecommendAreaBinding
+
+class RecommendAreaAdapter(private val onItemClickListener: (ArrayList<CategoryDetailArea>) -> Unit) :
+    ListAdapter<CategoryDetailArea, RecommendAreaHolder>(MyItemCallback()) {
+
+    class MyItemCallback : DiffUtil.ItemCallback<CategoryDetailArea>() {
+        override fun areItemsTheSame(
+            oldItem: CategoryDetailArea,
+            newItem: CategoryDetailArea
+        ): Boolean {
+            return oldItem.area == newItem.area
+        }
+
+        override fun areContentsTheSame(
+            oldItem: CategoryDetailArea,
+            newItem: CategoryDetailArea
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecommendAreaHolder {
+        val binding =
+            ItemRecommendAreaBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RecommendAreaHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecommendAreaHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item, onItemClickListener)
+    }
+}
+
+class RecommendAreaHolder(val binding: ItemRecommendAreaBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    private var selectedArea = arrayListOf<CategoryDetailArea>()
+
+    fun bind(
+        item: CategoryDetailArea,
+        onItemClickListener: (ArrayList<CategoryDetailArea>) -> Unit
+    ) {
+        binding.apply {
+            this.root.setOnClickListener {
+                applySelection(binding, item)
+                onItemClickListener(selectedList())
+            }
+
+            this.textArea.text = item.area
+        }
+    }
+
+    private fun selectedList(): ArrayList<CategoryDetailArea> {
+        return selectedArea
+    }
+
+    private fun applySelection(
+        binding: ItemRecommendAreaBinding,
+        categoryDetailArea: CategoryDetailArea
+    ) {
+        if (selectedArea.contains(categoryDetailArea)) {
+            selectedArea.remove(categoryDetailArea)
+            changeTextColor(binding, R.color.gray_B7B7B7)
+            binding.textArea.setBackgroundResource(R.drawable.round_16_gray_white)
+        } else {
+            selectedArea.add(categoryDetailArea)
+            changeTextColor(binding, R.color.white)
+            binding.textArea.setBackgroundResource(R.drawable.round_16_black)
+        }
+    }
+
+    private fun changeTextColor(binding: ItemRecommendAreaBinding, resId: Int) {
+        binding.textArea.setTextColor(
+            ContextCompat.getColor(
+                binding.root.context,
+                resId
+            )
+        )
+    }
+}
--- presentation/src/main/java/com/sypark/openTicket/view/fragments/RecommendFragment.kt
+++ presentation/src/main/java/com/sypark/openTicket/view/fragments/RecommendFragment.kt
@@ -1,21 +1,40 @@
 package com.sypark.openTicket.view.fragments
 
+import android.util.Log
 import android.view.View
-import androidx.navigation.fragment.findNavController
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexWrap
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
+import com.sypark.openTicket.Common
 import com.sypark.openTicket.R
 import com.sypark.openTicket.base.BaseFragment
 import com.sypark.openTicket.databinding.FragmentRecommendBinding
+import com.sypark.openTicket.view.adapter.RecommendAreaAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RecommendFragment : BaseFragment<FragmentRecommendBinding>(R.layout.fragment_recommend) {
 
-    override fun init(view: View) {
+    private lateinit var recommendAreaAdapter: RecommendAreaAdapter
 
-        binding.textHome.setOnClickListener {
-            findNavController().navigate(RecommendFragmentDirections.actionRecommendFragmentToMainFragment())
+    override fun init(view: View) {
+        // todo_sypark https://salix97.tistory.com/268 참고
+        FlexboxLayoutManager(view.context).apply {
+            flexWrap = FlexWrap.WRAP
+            flexDirection = FlexDirection.ROW
+            justifyContent = JustifyContent.CENTER
+        }.let {
+            binding.recyclerviewArea.apply {
+                layoutManager = it
+                recommendAreaAdapter = RecommendAreaAdapter {
+                    Log.e("!!!!", it.toString())
+                }
+
+                recommendAreaAdapter.submitList(Common.categoryDetailAreaList)
+                adapter = recommendAreaAdapter
+            }
         }
 
     }
-
 }
