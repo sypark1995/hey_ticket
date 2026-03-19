408ad615d06d8c3762ed6b43945d66a72c5e40ea
sypark1995 <115516297+sypark1995@users.noreply.github.com>
2026-03-19 19:05:08 +0900

    feat 회원가입 키워드 UI 생성 중 / 50%

---
+++ presentation/src/main/java/com/sypark/openTicket/view/fragments/RecommendKeywordFragment.kt
@@ -0,0 +1,15 @@
+package com.sypark.openTicket.view.fragments
+
+import android.view.View
+import com.sypark.openTicket.R
+import com.sypark.openTicket.base.BaseFragment
+import com.sypark.openTicket.databinding.FragmentRecommendKeywordBinding
+
+class RecommendKeywordFragment :
+    BaseFragment<FragmentRecommendKeywordBinding>(R.layout.fragment_recommend_keyword) {
+
+    override fun init(view: View) {
+
+    }
+
+}
