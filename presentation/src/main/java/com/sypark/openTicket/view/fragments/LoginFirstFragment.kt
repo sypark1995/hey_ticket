a3f0219413dd2b24ca2263397058181284c1d575
sypark1995 <115516297+sypark1995@users.noreply.github.com>
2026-04-20 20:30:59 +0900

    feat : soft keyboard handling

--- presentation/src/main/java/com/sypark/openTicket/view/fragments/LoginFirstFragment.kt
+++ presentation/src/main/java/com/sypark/openTicket/view/fragments/LoginFirstFragment.kt
@@ -1,72 +1,120 @@
 package com.sypark.openTicket.view.fragments
 
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.KeyEvent
+import android.view.KeyEvent.KEYCODE_ENTER
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.sypark.openTicket.R
 import com.sypark.openTicket.base.BaseFragment
 import com.sypark.openTicket.databinding.FragmentLoginFirstBinding
+import com.sypark.openTicket.excensions.dpToPx
+import com.sypark.openTicket.excensions.hide
+import com.sypark.openTicket.excensions.setMargins
+import com.sypark.openTicket.excensions.show
 import com.sypark.openTicket.model.LoginFirstViewModel
+import com.sypark.openTicket.util.KeyboardVisibilityUtils
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFirstFragment : BaseFragment<FragmentLoginFirstBinding>(R.layout.fragment_login_first) {
 
     private val viewModel: LoginFirstViewModel by viewModels()
+    private lateinit var keyboardVisibilityUtils: KeyboardVisibilityUtils
 
     override fun init(view: View) {
+        binding.apply {
+            viewModel.emailAddress.observe(viewLifecycleOwner) {
+                if (it.isEmpty()) {
+                    layoutLoginEdit.setBackgroundResource(R.drawable.round_12_gray_white)
+                    textEmailError.hide()
 
-        viewModel.emailAddress.observe(viewLifecycleOwner) {
-            if (it.isEmpty()) {
-                binding.layoutLoginEdit.setBackgroundResource(R.drawable.round_12_gray_white)
-                binding.textEmailError.visibility = View.GONE
+                    btnEmail.setBackgroundResource(R.drawable.round_12_gray)
+                    btnEmail.isEnabled = false
 
-                binding.btnEmail.setBackgroundResource(R.drawable.round_12_gray)
-                binding.btnEmail.isEnabled = false
-            } else {
-                if (android.util.Patterns.EMAIL_ADDRESS.matcher(it).matches()) {
-                    //email 맞음!
-                    binding.layoutLoginEdit.setBackgroundResource(R.drawable.round_12_gray_white)
-
-                    binding.btnEmail.setBackgroundResource(R.drawable.round_12_black)
-                    binding.btnEmail.isEnabled = true
-
-                    binding.textEmailError.visibility = View.GONE
                 } else {
-                    //email 아님
-                    binding.layoutLoginEdit.setBackgroundResource(R.drawable.round_12_red_white)
-                    binding.textEmailError.visibility = View.VISIBLE
 
-                    binding.btnEmail.setBackgroundResource(R.drawable.round_12_gray)
-                    binding.btnEmail.isEnabled = false
+                    if (android.util.Patterns.EMAIL_ADDRESS.matcher(it).matches()) {
+                        //email 맞음!
+                        layoutLoginEdit.setBackgroundResource(R.drawable.round_12_gray_white)
+
+                        btnEmail.setBackgroundResource(R.drawable.round_12_black)
+                        btnEmail.isEnabled = true
+
+                        textEmailError.hide()
+                    } else {
+                        //email 아님
+                        layoutLoginEdit.setBackgroundResource(R.drawable.round_12_red_white)
+                        textEmailError.show()
+
+                        btnEmail.setBackgroundResource(R.drawable.round_12_gray)
+                        btnEmail.isEnabled = false
+                    }
                 }
             }
-        }
 
-        binding.layoutLoginTop.imgBack.setOnClickListener {
-            findNavController().popBackStack()
-        }
-
-        binding.btnEmail.setOnClickListener {
-            viewModel.getLoginValidation(::isLoginValidation)
-        }
-
-        binding.editEmail.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            layoutLoginTop.imgBack.setOnClickListener {
+                findNavController().popBackStack()
             }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                viewModel.setEmailAddress(s.toString())
+            btnEmail.setOnClickListener {
+                viewModel.getLoginValidation(::isLoginValidation)
             }
 
-            override fun afterTextChanged(s: Editable?) {
+            editEmail.apply {
+                addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(
+                        s: CharSequence?,
+                        start: Int,
+                        count: Int,
+                        after: Int
+                    ) {
+                    }
 
+                    override fun onTextChanged(
+                        s: CharSequence?,
+                        start: Int,
+                        before: Int,
+                        count: Int
+                    ) {
+                        viewModel.setEmailAddress(s.toString())
+                    }
+
+                    override fun afterTextChanged(s: Editable?) {
+
+                    }
+                })
+
+                setOnKeyListener { v, keyCode, event ->
+                    if (event.action == KeyEvent.ACTION_DOWN && keyCode == KEYCODE_ENTER) {
+                        val imm =
+                            v.context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                        imm.hideSoftInputFromWindow(this.windowToken, 0)
+                    }
+                    true
+                }
             }
-        })
 
+            keyboardVisibilityUtils = KeyboardVisibilityUtils(requireActivity().window,
+                onShowKeyboard = { _, _ ->
+                    // 키보드가 올라올 때의 동작
+                    btnKakao.setMargins(bottom = 10.dpToPx())
+                },
+                onHideKeyboard = {
+                    // 키보드가 내려갈 때의 동작
+                    btnKakao.setMargins(bottom = 50.dpToPx())
+                }
+            )
+        }
+    }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        keyboardVisibilityUtils.detachKeyboardListeners()
     }
 
     private fun isLoginValidation(isLoginValidation: Boolean) {
