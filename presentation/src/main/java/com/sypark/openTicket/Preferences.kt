0261c931d285329a65d3490d7a9c2dbcdc1d40af
sypark1995 <115516297+sypark1995@users.noreply.github.com>
2026-04-20 20:29:19 +0900

    feat : sharedPreferences 추가

--- presentation/src/main/java/com/sypark/openTicket/Preferences.kt
+++ presentation/src/main/java/com/sypark/openTicket/Preferences.kt
@@ -6,17 +6,17 @@ import android.content.SharedPreferences
 
 //todo_sypark dataStore로 변경
 object Preferences {
-    private const val FILENAME = "com.sypark.openTicket"
     private lateinit var preferences: SharedPreferences
-
-
-    private val PREF_SORT_INDEX = "sortIndex"
 
     fun init(context: Context) {
         preferences = context.getSharedPreferences(FILENAME, Activity.MODE_PRIVATE)
     }
 
     var sortPosition: Int
-        get() = preferences.getInt(PREF_SORT_INDEX, 1)
-        set(value) = preferences.edit().putInt(PREF_SORT_INDEX, value).apply()
+        get() = preferences.getInt(PREFERENCE_SORT_INDEX, 1)
+        set(value) = preferences.edit().putInt(PREFERENCE_SORT_INDEX, value).apply()
+//
+//    var email: String
+//        get() = preferences.getString(PREFERENCE_KEY_EMAIL, "")
+//        set(value) = preferences.edit().putInt(PREFERENCE_KEY_EMAIL, value).apply()
 }
