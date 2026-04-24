57f3698f940aa9a8c42458b9d3647f398f4de4ff
seungyunpark <55783172+sypark95@users.noreply.github.com>
2026-04-24 16:50:10 +0900

    refactor : FindPassword UI observer,watcher refactoring

--- presentation/src/main/java/com/sypark/openTicket/view/fragments/LoginFindPwFragment.kt
+++ presentation/src/main/java/com/sypark/openTicket/view/fragments/LoginFindPwFragment.kt
@@ -1,7 +1,5 @@
 package com.sypark.openTicket.view.fragments
 
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -9,6 +7,7 @@ import androidx.navigation.fragment.navArgs
 import com.sypark.openTicket.R
 import com.sypark.openTicket.base.BaseFragment
 import com.sypark.openTicket.databinding.FragmentFindPwBinding
+import com.sypark.openTicket.excensions.onTextChanged
 import com.sypark.openTicket.model.LoginFindViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,52 +18,49 @@ class LoginFindPwFragment : BaseFragment<FragmentFindPwBinding>(R.layout.fragmen
     private val viewModel: LoginFindViewModel by viewModels()
 
     override fun init(view: View) {
-        binding.editEmail.setText(args.item)
+        setUpObserver()
 
         viewModel.setEmail(args.item)
+        binding.apply {
+            editEmail.setText(args.item)
 
-        binding.layoutLoginTop.imgBack.setOnClickListener {
-            findNavController().popBackStack()
+            layoutLoginTop.imgBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
+
+            editEmail.onTextChanged {
+                viewModel.setEmail(it)
+            }
         }
+    }
 
-        binding.editEmail.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    private fun setUpObserver() {
+        viewModel.email.observe(viewLifecycleOwner, ::emailWatcher)
+    }
 
-            }
+    private fun emailWatcher(email: String) {
+        if (email.isEmpty()) {
+            binding.layoutLoginEdit.setBackgroundResource(R.drawable.round_12_gray_white)
+            binding.textEmailError.visibility = View.GONE
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                viewModel.setEmail(s.toString())
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-            }
-
-        })
-
-        viewModel.email.observe(this) {
-            if (it.isEmpty()) {
+            binding.btnNext.setBackgroundResource(R.drawable.round_12_gray)
+            binding.btnNext.isEnabled = false
+        } else {
+            if (android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                //email 맞음!
                 binding.layoutLoginEdit.setBackgroundResource(R.drawable.round_12_gray_white)
+
+                binding.btnNext.setBackgroundResource(R.drawable.round_12_black)
+                binding.btnNext.isEnabled = true
+
                 binding.textEmailError.visibility = View.GONE
+            } else {
+                //email 아님!
+                binding.layoutLoginEdit.setBackgroundResource(R.drawable.round_12_red_white)
+                binding.textEmailError.visibility = View.VISIBLE
 
                 binding.btnNext.setBackgroundResource(R.drawable.round_12_gray)
                 binding.btnNext.isEnabled = false
-            } else {
-                if (android.util.Patterns.EMAIL_ADDRESS.matcher(it).matches()) {
-                    //email 맞음!
-                    binding.layoutLoginEdit.setBackgroundResource(R.drawable.round_12_gray_white)
-
-                    binding.btnNext.setBackgroundResource(R.drawable.round_12_black)
-                    binding.btnNext.isEnabled = true
-
-                    binding.textEmailError.visibility = View.GONE
-                } else {
-                    //email 아님!
-                    binding.layoutLoginEdit.setBackgroundResource(R.drawable.round_12_red_white)
-                    binding.textEmailError.visibility = View.VISIBLE
-
-                    binding.btnNext.setBackgroundResource(R.drawable.round_12_gray)
-                    binding.btnNext.isEnabled = false
-                }
             }
         }
     }
