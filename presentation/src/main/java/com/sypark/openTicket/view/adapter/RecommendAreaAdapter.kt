1fdacfb11b5b20300731b5278bea5fc256b55eb7
seungyunpark <55783172+sypark95@users.noreply.github.com>
2026-07-17 14:34:55 +0900

    feat : 관심 장르/지역 다중 선택 어댑터 추가

---
+++ presentation/src/main/java/com/sypark/openTicket/view/adapter/RecommendAreaAdapter.kt
@@ -0,0 +1,64 @@
+package com.sypark.openTicket.view.adapter
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.sypark.data.db.entity.Areas
+import com.sypark.openTicket.R
+import com.sypark.openTicket.databinding.ItemRecommendAreaBinding
+
+class RecommendAreaAdapter : ListAdapter<Areas, RecommendAreaAdapter.ViewHolder>(DiffCallback()) {
+
+    private val selectedCodes = mutableSetOf<String>()
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setSelectedCodes(codes: Set<String>) {
+        selectedCodes.clear()
+        selectedCodes.addAll(codes)
+        notifyDataSetChanged()
+    }
+
+    fun selectedCodes(): Set<String> = selectedCodes.toSet()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemRecommendAreaBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item, selectedCodes.contains(item.code)) {
+            if (selectedCodes.contains(item.code)) {
+                selectedCodes.remove(item.code)
+            } else {
+                selectedCodes.add(item.code)
+            }
+            notifyItemChanged(position)
+        }
+    }
+
+    class ViewHolder(private val binding: ItemRecommendAreaBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Areas, isSelected: Boolean, onClick: () -> Unit) {
+            binding.textArea.apply {
+                text = item.name
+                setOnClickListener { onClick() }
+                if (isSelected) {
+                    setBackgroundResource(R.drawable.round_16_black)
+                    setTextColor(ContextCompat.getColor(context, R.color.white))
+                } else {
+                    setBackgroundResource(R.drawable.round_16_gray_white)
+                    setTextColor(ContextCompat.getColor(context, R.color.gray_949494))
+                }
+            }
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<Areas>() {
+        override fun areItemsTheSame(oldItem: Areas, newItem: Areas): Boolean = oldItem.code == newItem.code
+        override fun areContentsTheSame(oldItem: Areas, newItem: Areas): Boolean = oldItem == newItem
+    }
+}
